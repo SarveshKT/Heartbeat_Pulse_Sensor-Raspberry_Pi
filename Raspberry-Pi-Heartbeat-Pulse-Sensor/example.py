e5b81523296a2bcda21d00c4fb4534ed85fe98dd
SarveshKT <59925834+SarveshKT@users.noreply.github.com>
2021-08-15 15:42:20 +0530

    Add files via upload

---
+++ Raspberry-Pi-Heartbeat-Pulse-Sensor/example.py
@@ -0,0 +1,17 @@
+from pulsesensor import Pulsesensor
+import time
+from openpyxl import load_workbook
+
+p = Pulsesensor()
+p.startAsyncBPM()
+
+try:
+    while True:
+        bpm = p.BPM
+        if bpm > 0:
+            print("BPM: %d" % bpm)
+        else:
+            print("No Heartbeat found")
+        time.sleep(1)
+except:
+    p.stopAsyncBPM()
